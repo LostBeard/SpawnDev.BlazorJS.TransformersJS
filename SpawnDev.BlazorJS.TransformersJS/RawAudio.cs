427630e9ef9d0ca1c5482c9116c0df736a941e87
Todd Tanner <todd@spawndev.com>
2025-10-14 13:07:10 -0400

    Added RawAudio

---
+++ SpawnDev.BlazorJS.TransformersJS/RawAudio.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.JSInterop;
+using SpawnDev.BlazorJS.JSObjects;
+
+namespace SpawnDev.BlazorJS.TransformersJS
+{
+    /// <summary>
+    /// https://github.com/huggingface/transformers.js/blob/1538e3a1544a93ef323e41c4e3baef6332f4e557/src/utils/audio.js#L781
+    /// </summary>
+    public class RawAudio : JSObject
+    {
+        /// <inheritdoc/>
+        public RawAudio(IJSInProcessObjectReference _ref) : base(_ref) { }
+        /// <summary>
+        /// Create a new `RawAudio` object.
+        /// </summary>
+        /// <param name="audio">audio Audio data</param>
+        /// <param name="samplingRate">sampling_rate Sampling rate of the audio data</param>
+        public RawAudio(Float32Array audio, double samplingRate) : base(JS.New("Transformers.RawAudio", audio, samplingRate)) { }
+        /// <summary>
+        /// Convert the audio to a wav file buffer.
+        /// </summary>
+        /// <returns>The WAV file.</returns>
+        public ArrayBuffer ToWav() => JSRef!.Call<ArrayBuffer>("toWav");
+        /// <summary>
+        /// Convert the audio to a WAV Blob.
+        /// </summary>
+        /// <returns></returns>
+        public Blob ToBlob() => JSRef!.Call<Blob>("toBlob");
+        /// <summary>
+        /// Save the audio to a wav file.
+        /// </summary>
+        /// <param name="path">path</param>
+        /// <returns></returns>
+        public Task Save(string path) => JSRef!.CallVoidAsync("save", path);
+    }
+}
